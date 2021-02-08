2c9d29435fcf464b6a27ceabe1b3361249672895
joq62 <joakim.leche@gmail.com>
2021-02-08 22:14:18 +0100

    ud

--- src/temp.erl
+++ src/temp.erl
@@ -1,37 +1,195 @@
 %%% -------------------------------------------------------------------
-%%% Author  : uabjle
-%%% Description : dbase using dets 
+%%% @author  : Joq Erlang
+%%% @doc: : 
+%%% Manage Computers
 %%% 
-%%% Created : 10 dec 2012
+%%% Created : 
 %%% -------------------------------------------------------------------
 -module(temp).  
-   
+
+-behaviour(gen_server).
 %% --------------------------------------------------------------------
 %% Include files
 %% --------------------------------------------------------------------
-
-%%---------------------------------------------------------------------
-%% Records for test
-%%
+%-include("timeout.hrl").
+%-include("log.hrl").
 
 %% --------------------------------------------------------------------
--compile(export_all).
+
+%% --------------------------------------------------------------------
+%% Key Data structures
+%% 
+%% --------------------------------------------------------------------
+-record(state, {current_temp,
+	        update_time}).
+
+-define(DefaultUpdateTime,60*1000).
+
+%% --------------------------------------------------------------------
+%% Definitions 
+%% --------------------------------------------------------------------
+
+% OaM related
+
+-export([read/0,
+	 set_temp/1,
+	 set_update_time/1,
+	 update/0
+	]).
+
+-export([start/0,
+	 stop/0,
+	 ping/0
+	]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3,handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 
 %% ====================================================================
 %% External functions
 %% ====================================================================
 
+%% Asynchrounus Signals
+
+
+%% Gen server functions
+
+start()-> gen_server:start_link(?MODULE, [], []).
+stop()-> gen_server:call(?MODULE, {stop},infinity).
+
+
+
+%%-----------------------------------------------------------------------
+read()-> 
+    gen_server:call(?MODULE, {read},infinity).
+
+
+ping()-> 
+    gen_server:call(?MODULE, {ping},infinity).
+
+%%----------------------------------------------------------------------
+set_temp(Temp)-> 
+    gen_server:cast(?MODULE, {set_temp,Temp}).
+set_update_time(Time)-> 
+    gen_server:cast(?MODULE, {set_update_time,Time}).
+update()-> 
+    gen_server:cast(?MODULE, {update}).
+
+
+%% ====================================================================
+%% Server functions
+%% ====================================================================
+
 %% --------------------------------------------------------------------
-%% Function:start/0 
-%% Description: Initiate the eunit tests, set upp needed processes etc
-%% Returns: non
+%% Function: init/1
+%% Description: Initiates the server
+%% Returns: {ok, State}          |
+%%          {ok, State, Timeout} |
+%%          ignore               |
+%%          {stop, Reason}
+%
 %% --------------------------------------------------------------------
+init([]) ->
+    spawn(fun()->l_update(?DefaultUpdateTime) end),
+    {ok, #state{current_temp=not_updated,
+	       update_time=?DefaultUpdateTime}}.
+    
+%% --------------------------------------------------------------------
+%% Function: handle_call/3
+%% Description: Handling call messages
+%% Returns: {reply, Reply, State}          |
+%%          {reply, Reply, State, Timeout} |
+%%          {noreply, State}               |
+%%          {noreply, State, Timeout}      |
+%%          {stop, Reason, Reply, State}   | (terminate/2 is called)
+%%          {stop, Reason, State}            (aterminate/2 is called)
+%% --------------------------------------------------------------------
+handle_call({ping},_From,State) ->
+    Reply={pong,node(),?MODULE},
+    {reply, Reply, State};
+
+handle_call({read},_From,State) ->
+    Reply=State#state.current_temp,
+    {reply, Reply, State};
+
+handle_call({stop}, _From, State) ->
+    {stop, normal, shutdown_ok, State};
+
+handle_call(Request, From, State) ->
+    Reply = {unmatched_signal,?MODULE,Request,From},
+    {reply, Reply, State}.
+
+%% --------------------------------------------------------------------
+%% Function: handle_cast/2
+%% Description: Handling cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% -------------------------------------------------------------------
+handle_cast({set_temp,Temp}, State) ->
+    {noreply, State#state{current_temp=Temp}};
+
+
+handle_cast({set_update_time,Time}, State) ->
+    {noreply, State#state{update_time=Time}};
+
+handle_cast({update}, State) ->
+    io:format("update temp  ~p~n",[{?MODULE,?LINE}]),
+    spawn(fun()->l_update(State#state.update_time) end),
+    {noreply, State};
 
     
-    
+handle_cast(Msg, State) ->
+    io:format("unmatched match cast ~p~n",[{?MODULE,?LINE,Msg}]),
+    {noreply, State}.
+
 %% --------------------------------------------------------------------
-%% Function:start/0 
-%% Description: Initiate the eunit tests, set upp needed processes etc
+%% Function: handle_info/2
+%% Description: Handling all non call/cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+
+handle_info(Info, State) ->
+    io:format("unmatched match info ~p~n",[{?MODULE,?LINE,Info}]),
+    {noreply, State}.
+
+
+%% --------------------------------------------------------------------
+%% Function: terminate/2
+%% Description: Shutdown the server
+%% Returns: any (ignored by gen_server)
+%% --------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%% --------------------------------------------------------------------
+%% Func: code_change/3
+%% Purpose: Convert process state when code is changed
+%% Returns: {ok, NewState}
+%% --------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%% --------------------------------------------------------------------
+%%% Internal functions
+%% --------------------------------------------------------------------
+%% --------------------------------------------------------------------
+%% Function: 
+%% Description:
+%% Returns: non
+%% --------------------------------------------------------------------
+l_update(Interval)->
+    timer:sleep(Interval),
+    rpc:cast(node(),temp,update,[]).
+%% --------------------------------------------------------------------
+%% Internal functions
+%% --------------------------------------------------------------------
+
+%% --------------------------------------------------------------------
+%% Function: 
+%% Description:
 %% Returns: non
 %% --------------------------------------------------------------------
